7408e5e8599b4d00a89df182cbb2942452f98e4b
Shashank-MP <shashankmechana2001@gmail.com>
2023-01-25 16:21:55 +0530

    first commit

---
+++ src/services/upload-files.service.js
@@ -0,0 +1,22 @@
+import http from "../http-common";
+
+class UploadFilesService {
+  upload(file, onUploadProgress) {
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return http.post("http://localhost:8080/product/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  }
+
+  getFiles() {
+    return http.get("http://localhost:8080/product");
+  }
+}
+
+export default new UploadFilesService();
